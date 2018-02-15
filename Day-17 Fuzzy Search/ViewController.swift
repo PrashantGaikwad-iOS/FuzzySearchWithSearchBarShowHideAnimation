078c0dc967e93ed2786f6e640602a421387ad7df
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2018-02-15 15:20:32 +0530

    Initial Commit

---
+++ Day-17 Fuzzy Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day-17 Fuzzy Search
+//
+//  Created by Prashant Gaikwad on 2/15/18.
+//  Copyright © 2018 Prashant Gaikwad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Day-17 Fuzzy SearchTests/Day_17_Fuzzy_SearchTests.swift
@@ -0,0 +1,36 @@
+//
+//  Day_17_Fuzzy_SearchTests.swift
+//  Day-17 Fuzzy SearchTests
+//
+//  Created by Prashant Gaikwad on 2/15/18.
+//  Copyright © 2018 Prashant Gaikwad. All rights reserved.
+//
+
+import XCTest
+@testable import Day_17_Fuzzy_Search
+
+class Day_17_Fuzzy_SearchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
